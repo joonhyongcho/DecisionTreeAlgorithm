db5a1edae8c0b436e433b4448134475dcd43e17d
joonhyongcho <joonhyongcho@gmail.com>
2016-11-20 10:10:26 -0500

    Add readme and different dataset

--- README.md
+++ README.md
@@ -2,21 +2,41 @@
 
 This python application builds a decision tree from the flags dataset at the uci.edu datasets archive. http://archive.ics.uci.edu/ml/datasets/Flags. It creates a decision tree from the data and runs a few classification tests. This application is built for Dartmouth's Intro to Computation Neuroscience class, taught by Professor Granger.
 
+## Installation
+
+To run the visualization components of the program, you must install the PIL library, which can be installed by running `pip install image` on the terminal. You must have `pip` installed in order to retrieve PIL. You may find that pip does not install the PIL library properly, in which case you should run `python -m pip install image`, which will install the libraries in the folder of your default python interpreter.
+
 ## Algorithm Specifics
 
 Again, this program implements the decision tree learning algorithm, which classifies instances of a dataset by traversing down a decision tree until reaching a leaf node, which provides the classification value. At each node, the classification algorithm tests an attribute of the instance, then traverses down the appropriate branch to the node which contains the value of the tested attribute. This step continues until we have reached a leaf node.
 
 ## Implementation
 
-The algorithm is implemented in python, with a series of functions that 1. parse the input data 2. create subtrees 3. calculate entropy and 4. builds the entire tree and 5. classifies instances.
+The algorithm is implemented in python, and the program runs in two steps: Build the decision tree, then classify instances of the data set.
 
-In general, the algorithm learns decision trees by constructing them top down. It decides which attribute to use at any certain level, where the the root is at level 0, it’s direct children at level1, and the children of those children at level 2 and so on and so forth. Each instance attribute is evaluated using a statistical test to determine how it classifies the training examples on its own. Then, a descendant of the root is selected and created for every possible value of the attribute. Then, the same test is used at each of the children node, where the training examples have been filtered, to select the next children.
+### Building the Decision Tree
+
+The algorithms recursively constructs the decision tree from top to bottom, starting from the root, which looks at all of the rows in the dataset. At each iteration of the `buildTree` method, the algorithm calculates the attribute that would "best" split the current rows. In this dataset, each column represents a different attribute. The column number, the attribute represented at that column, and possible values, which are represented by numbers in the dataset, are listed below:
+1. age of the patient: (1) young, (2) pre-presbyopic, (3) presbyopic
+2. spectacle prescription:  (1) myope, (2) hypermetrope
+3. astigmatic:     (1) no, (2) yes
+4. tear production rate:  (1) reduced, (2) normal
+
+And so, `buildTree` will determine whether to split the current data by tear production rate or age of the patient. After deciding which attribute to use, the algorithm then splits the data into disjoint sets, where each set shares the same value for that attribute. It then runs `buildTree` on each set and adds the resulting subtrees to the children array of the current decision node.
+
+The "best" split of the data is determined by a statistical property called *information gain*. It measures how well a given attribute separates the training examples according to a target classification. The target classification in our case is whether the patient should be fitted with 1. hard contact lenses 2. soft contact lenses or 3. no lenses at all. Information gain relies on entropy, which measures the "impurity" of a collection of examples. More information on entropy can be found here[!https://en.wikipedia.org/wiki/Entropy_(information_theory)].
+
+Entropy is calculated by the equation:
+![description](entropy.jpg)
+
 
 To test the which attribute to test at each node in the tree, we need to select the attribute that is most useful for classifying the examples. We use a property, called information gain, that measures how well a given attribute separates the training examples according to their target classification. To this end, we use entropy, which measures the impurity of a collection of examples. Given a collection S, containing positive and negative examples of a target concept, the entropy of S relative to boolean classification is
 
 Information gain is the expected reduction in entropy caused by partitioning the samples according to the attribute. Gain(S,A) is Entropy(S) - sum of values Sv over S * entropy(Sv)
 
 Values(A) is set of all possible values for attribute A, and Sv is the subset of S for which attribute A has value v.
+
+In the future, this could be made more generic, but the program is specific to the flag dataset because a lot of the printing methods utilize a predetermine dictionary that maps column indices to certain real life values based on the value. For example, the dictionary includes the entry:  5: [English, Spanish, French, German, Slavic, Other Indo-European, Chinese, Arabic, Japanese/Turkish/Finnish/Magyar, Others]. This could be made more generic as to parse all dataset.names.txt files.
 
 https://www.cs.princeton.edu/courses/archive/spring07/cos424/papers/mitchell-dectrees.pdf
 
--- decisionTree.py
+++ decisionTree.py
@@ -1,104 +1,225 @@
+from __future__ import print_function
+from math import log
+from collections import deque
+from PIL import Image,ImageDraw
+
 # religion of country from size and colours in flag
 # http://gabrielelanaro.github.io/blog/2016/03/03/decision-trees.html
 # http://www.patricklamle.com/Tutorials/Decision%20tree%20python/tuto_decision%20tree.html
 #
-class decisionnode:
-  def __init__(self,col=-1,value=None,results=None,tb=None,fb=None):
+#
+
+# These arrays convert the number values in the data set to the Attributes they describe in English
+lenseAttributes = [
+    ("Age of Patient", ["Young", "Pre-presbyopic", "Presbyopic"]),
+    ("Spectacle Prescription", ["Myope", "Hypermetrope"]),
+    ("Astigmatic", ["No", "Yes"]),
+    ("Tear Production Rate", ["Reduced", "Normal"])
+]
+
+classAttribute = ["Hard", "Soft", "Not Fitted"]
+
+# Decision Node class
+class DNode:
+  def __init__(self,col=-1,value=None,results=None,children=None):
     self.col=col
     self.value=value
     self.results=results
-    self.tb=tb
-    self.fb=fb
+    self.children = children
 
+# parseTextToArray parses the flagdata file and converts integer stings to integers
 def parseTextToArray():
 
     res = []
-    for line in open("flagSmall.txt", "r"):
-        line = line.strip()
-        line = line.split(",")
+    for line in open("lenses.data.txt", "r"):
+        line = line.split()
         line = [int(attr) if any(char.isdigit() for char in attr) else attr for attr in line]
         res.append(line)
     return res
 
-def divideSet(rows, column, value):
-    splitFunction = None
+# divideSetByAllValues creates maps values to the rows that contain that value for a given column
+def divideSetByCol(rows, column):
+    valueMap = {}
 
-    if isinstance(value, int) or isinstance(value, float):
-        splitFunction = lambda row:row[column] >= value
-    else:
-        splitFunction = lambda row:row[column] == value
-
-    set1 = [row for row in rows if splitFunction(row)]
-    set2 = [row for row in rows if not splitFunction(row)]
-    return (set1, set2)
-
-def entropy(rows):
-    from math import log
-    log2 = lambda x:log(x) / log(2)
-    results = uniqueCounts(rows)
-    entr = 0.0
-    for r in results.keys():
-        p = float(results[r])/len(rows)
-        ent = ent-p*log2(p)
-    return ent
-def uniqueCounts(rows, idx):
-    results = {}
     for row in rows:
-        r = row[idx]
-        if r not in results:
-            results[r] = 0
-        results[r] += 1
-    return results
+        if row[column] in valueMap:
+            valueMap[row[column]].append(row)
+        else:
+            valueMap[row[column]] = [row]
 
-def buildTree(rows, scoref=entropy):
+    return valueMap
+
+# calculateEntropy calculates the entropy for the rows grouping by certain column
+def calculateEntropy(rows, col):
+    entropy = 0.0
+    classMap = divideSetByCol(rows, col)
+
+    for label in classMap:
+        proportion = float(len(classMap[label]))/len(rows)
+        entropy -= proportion*log(proportion, 2)
+
+    return entropy
+
+def divideRowsByClass(rows, classIdx):
+    return divideSetByCol(rows, classIdx)
+
+def buildTree(rows, classIdx):
     if len(rows) == 0:
-        return decisionnode()
-    currentScore = scoref(rows)
+        return DNode()
+
+    currentScore = calculateEntropy(rows, classIdx)
 
     bestGain = 0.0
     bestCriteria = None
     bestSets=None
 
-    columnCount = len(rows[0]) - 1
+    for col in range(len(rows[0])):
+        # skip the class
+        if col == classIdx:
+            continue
 
-    for col in range(0, columnCount):
-        global columnValues
-        columnValues = {}
-        for row in rows:
-            columnValues[row[col]] = 1
-        for value in volumn_values.keys():
-            set1, set2 = divideSet(rows, col, value)
-            p = float(len(set1))/len(rows)
-            gain = currentScore - p *scoref(set1) - (1-p)* scoref(set2)
+        attrMap = divideSetByCol(rows, col)
+        gain = currentScore
 
-            if gain > bestGain and len(set1) > 0 and  len(set2) > 0:
-                bestGain = gain
-                bestCriteria = (col, value)
-                bestSets = (set1, set2)
-        if bestGain > 0:
-            truBranch = buildTree(bestSets[0])
-            falseBranch = buildTree(bestSets[1])
-            return decisionnode(col=bestCriteria[0], value=bestCriteria[1], tb=TruBranch, fb=FalseBranch)
-        else:
-            return decisionnode(results=uniqueCounts(rows))
+        for attr in attrMap:
+            proportion = float(len(attrMap[attr])) / len(rows)
+            gain -= proportion * calculateEntropy(attrMap[attr], classIdx)
+
+        if gain > bestGain:
+            bestGain = gain
+            bestCriteria = col
+            bestSets = attrMap
+
+    if bestGain > 0:
+        children = []
+        for value in bestSets:
+            childNode = buildTree(bestSets[value], classIdx)
+            childNode.value = value
+            children.append(childNode)
+        return DNode(col=bestCriteria, children=children)
+    else:
+        return DNode(results=divideRowsByClass(rows, classIdx))
+
 def classify(observation,tree):
     if tree.results!=None:
-        return tree.results
+        return tree.results.keys()[0]
     else:
-        v=observation[tree.col]
-        branch=None
-    if isinstance(v,int) or isinstance(v,float):
-        if v>=tree.value: branch=tree.tb
-        else: branch=tree.fb
-    else:
-        if v==tree.value: branch=tree.tb
-        else: branch=tree.fb
-    return classify(observation,branch)
+        value = observation[tree.col]
+        print("Checking attribute " +str(lenseAttributes[tree.col][0]) + " and val is " + str(lenseAttributes[tree.col][1][value-1]))
+        for child in tree.children:
+            if child.value == value:
+                return classify(observation, child)
 
-print(any(char.isdigit() for char in 'Afghanistan'))
-print(any(char.isdigit() for char in "American-Samoa"))
+    return None
+
+def getwidth(tree):
+    queue = deque()
+    queue.append((tree, 0))
+    maxLevel = 0
+    currLevel = 0
+    maxWidth = 0
+    currWidth = 0
+
+    while queue:
+        currTup = queue.popleft()
+        node = currTup[0]
+        level = currTup[1]
+
+        if level != currLevel and currWidth > maxWidth:
+            maxWidth = currWidth
+            maxLevel = currLevel
+        elif level != currLevel:
+            currLevel = level
+            currWidth = 1
+        else:
+            currWidth += 1
+
+        if node.children:
+            for child in node.children:
+                queue.append((child, level + 1))
+
+    return maxWidth
+
+def getdepth(tree):
+    queue = deque()
+    queue.append((tree, 0))
+
+    maxLevel = 0
+
+    while queue:
+        currTup = queue.popleft()
+        node = currTup[0]
+        level = currTup[1]
+
+        if level > maxLevel:
+            maxLevel = level
+
+        if node.children:
+            for child in node.children:
+                queue.append((child, level + 1))
+
+    return maxLevel
+
+def drawtree(tree,jpeg='tree.jpg'):
+    w = getwidth(tree) * 100
+    h = getdepth(tree) * 100 + 120
+
+    img=Image.new('RGB',(w,h),(255,255,255))
+    draw=ImageDraw.Draw(img)
+
+    drawnode(draw,tree,w/2,20)
+    img.save(jpeg,'JPEG')
+
+def drawnode(draw,tree,x,y):
+    if tree.results==None:
+        # Get the width of each branch
+        widths = []
+        for child in tree.chidren:
+            widths.append(getwidth(child) * 100)
+        sumWidths = sum(widths)
+        # w1=getwidth(tree.fb)*100
+        # w2=getwidth(tree.tb)*100
+
+        # Determine the total space required by this node
+        spaces = []
+        # for width in widths:
+            # if len(widths) % 2:
+
+
+        left=x-(sumWidths)/len(sumWidths)
+        right=x+(sumWidths)/len(sumWidths)
+
+        # Draw the condition string
+        draw.text((x-20,y-10),str(tree.col)+':'+str(tree.value),(0,0,0))
+
+        # Draw links to the branches
+        for width in tree.children:
+            draw.line((x,y, left+w1/2,y+100),fill=(255,0,0))
+        draw.line((x,y,left+w1/2,y+100),fill=(255,0,0))
+        draw.line((x,y,right-w2/2,y+100),fill=(255,0,0))
+
+        # Draw the branch nodes
+        drawnode(draw,tree.fb,left+w1/2,y+100)
+        drawnode(draw,tree.tb,right-w2/2,y+100)
+    else:
+        txt=' \n'.join(['%s:%d'%v for v in tree.results.items()])
+        draw.text((x-20,y),txt,(0,0,0))
+
+def printTree(tree,indent=''):
+    if tree.results!=None:
+        for key in tree.results:
+            print("Class: " + str(classAttribute[key-1]))
+    else:
+        print("Check Attribute: " + str(lenseAttributes[tree.col][0]))
+        if tree.children:
+            for child in tree.children:
+                print(indent + " " + "if " + str(lenseAttributes[tree.col][1][child.value-1]), end=" ")
+                # print(indent + " " + "if " + str(lenseAttributes[child.col][1][child.value-1]), end= " ")
+                printTree(child,indent+'  ')
+
 data = parseTextToArray()
-print(uniqueCounts(data, 6))
-set1, set2 = divideSet(data, 6, 4)
-print(set1)
-print(set2)
+# Exclude individual ids
+data = [data[i][1:] for i in range(len(data))]
+tree = buildTree(data, len(data[0]) - 1)
+printTree(tree)
+print(classAttribute[classify([2,  1,  1,  2,  2], tree)- 1])
